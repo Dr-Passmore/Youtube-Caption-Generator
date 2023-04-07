6217c4361edfeb87c610031a641697dbd473e461
Phillip Passmore <phillipspassmore@gmail.com>
2023-04-07 19:17:50 +0100

    user interface started, logging added
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- main.py
+++ main.py
@@ -14,34 +14,48 @@ import os
 
 class srtGeneration():
     def __init__(self):
+        #checks config.ini exists
         config = configparser.ConfigParser()
         configFile = os.path.exists('config.ini')
         if configFile == False:
-            print("No file found")
+            #if the file does not exist then create one
+            logging.info("No config file found")
             srtGeneration.configurationFileCreation(self, config)
-            
+        else:
+            logging.info("config.ini exists")
+        
+        #load user interface
+        srtGeneration.userInterface(self, config)    
+        
         
     def configurationFileCreation(self, config):
-        config.add_section('API Key')
-        config.set('API Key', 'Key', '')
-        with open(r"config.ini", 'w') as configuration:
-            config.write(configuration)
-        
-        
+        logging.info("Creating 'config.ini' file")
+        try:
+            config.add_section('API Key')
+            config.set('API Key', 'Key', '')
+            with open(r"config.ini", 'w') as configuration:
+                config.write(configuration)
+        except:
+            logging.error("Failed to create config.ini")
+    
+    def userInterface(self, config):
+        root = tkinter.Tk()
 
+        button = ttk.Button(root, text="Click me!")
+        button.pack()
+        sv_ttk.set_theme("dark")
+
+        root.mainloop()
 
 key = secret.access_key
 leopard = pvleopard.create(access_key=key)
 
-root = tkinter.Tk()
 
-button = ttk.Button(root, text="Click me!")
-button.pack()
+
+
 
 # This is where the magic happens
-sv_ttk.set_theme("dark")
 
-root.mainloop()
 '''
 with open("./videolist.csv", 'r') as file:
     csvreader = csv.reader(file)
