b89c1b503aebcfd00695111e16210f6f488d17df
Phillip Passmore <phillipspassmore@gmail.com>
2023-04-07 09:49:26 +0100

    Clean up from bulk processing - starting GUI
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+#import python files
 import downloadMP3
 import secret
 import createSRT
+
+#import python libraries
 import pvleopard
 import csv
 import time
+import tkinter
+from tkinter import ttk
+import sv_ttk
 
 key = secret.access_key
 leopard = pvleopard.create(access_key=key)
 
+root = tkinter.Tk()
+
+button = ttk.Button(root, text="Click me!")
+button.pack()
+
+# This is where the magic happens
+sv_ttk.set_theme("dark")
+
+root.mainloop()
+'''
 with open("./videolist.csv", 'r') as file:
     csvreader = csv.reader(file)
     for row in csvreader:
@@ -25,4 +41,4 @@ with open("./videolist.csv", 'r') as file:
         
         time.sleep(10)
 
-
+'''
